918ab788c59aa8581bc147ac25b0bda7f6df64bc
Lee <545360100@qq.com>
2018-10-09 00:47:26 +0800

    完成路由功能和post json解析

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# node-server
+# node-server 一个简单的静态服务器
+### 启动
+```
+启动服务器画
+node ./index.js 
+或者
+nodemon ./index.js
+```
+### 使用
+在routes中添加对应路由，如：
+```
+//通过http://localhost:8080/a?c=123&d=123  通过http://localhost:8080/postData 可以访问postData接口
+  "/a": function (req, res) {
+    res.end(JSON.stringify(req.query));
+  },
+  '/postData': function (req, res) {
+    console.log(req.body);
+    res.end(JSON.stringify(req.body))
+  },
+```
+其中GET方法的数据存储在req.query中，POST的数据存储在req.body中
+
+可以通过运行/sample目录下的b.html模拟POST方法，使用JSON格式进行数据传输
--- index.js
+++ index.js
@@ -7,41 +7,65 @@ const PORT = "8080";
 const static_dir = path.join(__dirname, "static");
 
 var routes = {
-  "/a": function(req, res) {
+  "/a": function (req, res) {
     res.end(JSON.stringify(req.query));
   },
-  "/b": function(req, res) {
+  "/b": function (req, res) {
     res.end("match /b");
   },
-  "/a/c": function(req, res) {
+  "/a/c": function (req, res) {
     res.end("match /a/c");
   },
-  "/search": function(req, res) {
-    res.end("username=" + req.body.username + ",password=" + req.body.password);
+  "/weather": function (req, res) {
+    res.end(JSON.stringify({ a: 1, b: 2 }));
+  },
+  '/postData': function (req, res) {
+    console.log(req.body);
+    res.end(JSON.stringify(req.body))
+  },
+  "/search": function (req, res) {
+    try {
+      res.end("username=" + req.body.username + ",password=" + req.body.password);
+    }
+    catch (err) { res.end('search') }
   }
 };
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function (req, res) {
   routePath(req, res);
 });
 
 function routePath(req, res) {
   var pathObj = url.parse(req.url, true);
   var handleFn = routes[pathObj.pathname];
-  console.log(Boolean(handleFn));
   if (handleFn) {
-    console.log("pathObj", pathObj.query);
-    console.log("query", req.query);
-    handleFn(req, res);
+    req.query = pathObj.query
+    console.log('req.query:', req.query);
+    // 官方写法
+    // 参考 https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
+    let body = []
+    req.on('data', chunk => {
+      body.push(chunk);
+    }).on('end', () => {
+      try {
+      req.body = JSON.parse(Buffer.concat(body).toString())
+        handleFn(req, res);
+      }
+      catch (err) { handleFn(req, res); }
+
+    })
+
   }
-  console.log(req);
-  res.end(JSON.stringify(req));
+  else {
+    getStaticFile(req, res)
+  };
 }
 
 function getStaticFile(req, res) {
   let pathname = url.parse(req.url).pathname;
   let filePath = path.join(static_dir, pathname);
-  fs.readFile(filePath, "binary", function(err, content) {
+  console.log(filePath);
+  fs.readFile(filePath, "binary", function (err, content) {
     if (err) {
       res.writeHead("404", "haha Not Found");
       return res.end();
